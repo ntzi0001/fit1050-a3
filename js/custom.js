f9ca5ddc683b8fdccbaa1bcc0af0d2b3b07e2d37
Nick T <nick.tzimos@protonmail.com>
2023-05-23 23:18:46 +1000

    add correctimages to slideshow, fix title read

--- js/custom.js
+++ js/custom.js
@@ -5,9 +5,8 @@
 
     // HERO SLIDE
     $('.hero-slide').backstretch([
-      "images/slideshow/white-wall-living-room-have-sofa-decoration-3d-rendering.jpg", 
-      "images/slideshow/interior-wall-mockup-with-sofa-cabinet-living-room-with-empty-white-wall-background-3d-rendering.jpg",
-      "images/slideshow/wood-sideboard-living-room-interior-with-copy-space.jpg"
+      "../images/slideshow/commodore.jpg",
+      "../images/slideshow/two_minis.jpg"
     ],  {duration: 2000, fade: 750});
 
     // REVIEWS CAROUSEL
